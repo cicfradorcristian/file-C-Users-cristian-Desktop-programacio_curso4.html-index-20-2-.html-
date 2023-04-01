e29f0f8eb4d50d5d32e376b837b6b0ee321fecc7
cicfradorcristian <129440840+cicfradorcristian@users.noreply.github.com>
2023-04-01 19:41:59 -0300

    initial commit

---
+++ formulario.js
@@ -0,0 +1,7 @@
+
+alert("Bienvenido")
+document.getElementById('boton').onclick= function() {
+	console.log("capturamos el evento click");
+document.getElementById("demo").innerHTML="Gracias por contactarse";
+}
+
